0f1e31095a4148742b3463aa6a5433714615bcaa
Mrinal Gupta <mrinalgupta1097@gmail.com>
2021-10-12 14:01:30 +0530

    Callable example.

---
+++ Multithreading/src/executerframework/CallableDemo.java
@@ -0,0 +1,30 @@
+package executerframework;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CallableDemo {
+  public static void main(String[] args) {
+    //
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+/*
+* submit() also takes callable and returns a future object*/
+    Future future =
+        executorService.submit(
+            () -> System.out.println(Thread.currentThread().getName() + " : Task1.2"));
+
+    System.out.println(future.isDone());
+
+    try{
+      String result = (String) future.get();
+      System.out.println(result);
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    System.out.println(future.isDone());
+  }
+}
