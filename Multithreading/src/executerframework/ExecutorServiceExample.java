c21a67c83e364bc4523e36593c83d1c0e06f3347
Mrinal Gupta <mrinalgupta1097@gmail.com>
2021-10-12 13:47:56 +0530

    Runnable and Future, submit() method and code formatting.

--- Multithreading/src/executerframework/ExecutorServiceExample.java
+++ Multithreading/src/executerframework/ExecutorServiceExample.java
@@ -4,24 +4,34 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ExecutorServiceExample {
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+  public static void main(String[] args) {
+    /*
+     * An Executor that provides methods to manage termination
+     * and methods that can produce a Future for tracking progress of one or more asynchronous tasks.
+     * An ExecutorService can be shut down, which will cause it to reject new tasks.
+     *  Two different methods are provided for shutting down an ExecutorService.*/
+    ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-        executorService.execute(newRunnable("Task1"));
-        executorService.execute(newRunnable("Task2"));
-        executorService.execute(newRunnable("Task3"));
+    executorService.execute(newRunnable("Task1"));
+    executorService.execute(newRunnable("Task2"));
+    executorService.execute(newRunnable("Task3"));
 
-        executorService.shutdown();
-    }
+    /*shutDown() method
+     * Initiates an orderly shutdown in which previously submitted tasks are executed,
+     *  but no new tasks will be accepted.
+     *  Invocation has no additional effect if already shut down.
+     *  This method does not wait for previously submitted tasks to complete execution.
+     *  Use awaitTermination to do that.*/
+    executorService.shutdown();
+  }
 
-    private static Runnable newRunnable(String message) {
-        return new Runnable() {
-            @Override
-            public void run() {
-                String completeMessage = Thread.currentThread().getName() + ": " + message;
-                System.out.println(completeMessage);
-            }
-        };
-    }
-
+  private static Runnable newRunnable(String message) {
+    return new Runnable() {
+      @Override
+      public void run() {
+        String completeMessage = Thread.currentThread().getName() + ": " + message;
+        System.out.println(completeMessage);
+      }
+    };
+  }
 }
---
+++ Multithreading/src/executerframework/ExecutorServiceMethodsDemo.java
@@ -0,0 +1,50 @@
+package executerframework;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ExecutorServiceMethodsDemo {
+  public static void main(String[] args) {
+    //
+    ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+    /*
+     * In order to create a pool of one thread use
+     * ExecutorService executorService = Executors.newSingleThreadExecutor*/
+
+    executorService.execute(
+        () -> System.out.println(Thread.currentThread().getName() + " : Task1.1"));
+    executorService.execute(
+        () -> System.out.println(Thread.currentThread().getName() + " : Task1.2"));
+    executorService.execute(
+        () -> System.out.println(Thread.currentThread().getName() + " : Task1.3"));
+
+    //   submit() method - Submits a Runnable task for execution and returns a Future representing
+    // that task. The Future's get method will return null upon successful completion.
+    Future future =
+        executorService.submit(
+            () -> System.out.println(Thread.currentThread().getName() + " : Task1.1"));
+
+    /*More About Future
+     * A Future represents the result of an asynchronous computation.
+     *  Methods are provided to check if the computation is complete, to wait for its completion,
+     *  and to retrieve the result of the computation.
+     *  The result can only be retrieved using method get when the computation has completed,
+     *  blocking if necessary until it is ready. Cancellation is performed by the cancel method. */
+    System.out.println(future.isDone());
+
+    try {
+      future.get(); // get() method returns null if the task is in progress state
+    } catch (InterruptedException e) {
+      System.out.println(e.getMessage());
+    } catch (ExecutionException e) {
+      System.out.println(e.getMessage());
+    }
+
+    System.out.println(future.isDone());
+
+    executorService.shutdown();
+  }
+}
